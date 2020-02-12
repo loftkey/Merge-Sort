40767d20e43468d6bf1ed2c29642c0db07d8c60d
Andrew Price <005475141@coyote.csusb.edu>
2020-02-12 10:54:47 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,79 +1,78 @@
-
 #include <iostream>
-#include <vector>
+#include <time.h>
 using namespace std;
-void merge(vector<int>& A, int start, int mid, int end);
-void merge_sort(vector<int>& A, int start, int end);
-void merge_sort_helper(vector<int>& A);
+
+void TopDownMergeSort(int A[], int B[], int n);
+void TopDownSplitMerge(int B[], int iBegin, int iEnd, int A[]);
+void TopDownMerge(int A[], int iBegin, int iMiddle, int iEnd, int B[]);
+void CopyArray(int A[], int iBegin, int iEnd, int B[]);
 
 int main() {
-
-    vector<int> scores;
-    scores.push_back(73);
-    scores.push_back(42);
-    scores.push_back(85);
-    scores.push_back(90);
-    scores.push_back(64);
-    scores.push_back(100);
-    scores.push_back(56);
-    scores.push_back(78);
-
-    cout << "Before: " << endl;
-    for (int i = 0; i < scores.size(); i++)
-        cout << scores[i] << endl;
-
-    
-    //merge_sort(scores, 0, 7);
-    merge_sort_helper(scores);
+    srand(time(NULL));
+    int arr1[10];
+    int arr2[10]{0};
+    for (auto& val : arr1) {
+        val = rand() % 101;
+    }
+   
+    cout << "Before sort" << endl;
+    for (auto i : arr1)
+        cout << i << ", ";
+    TopDownMergeSort(arr1, arr2, 10);
 
 
-    cout << endl <<  "After: " << endl;
-    for (int i = 0; i < scores.size(); i++)
-        cout << scores[i] << endl;
-
+    cout << endl << endl << "After sort" << endl;
+    for (auto i : arr1)
+        cout << i << ", ";
+    cout << endl;
     return 0;
 }
 
-void merge(vector<int> &A, int start, int mid, int end) {
-    //stores the starting position of both parts in temporary variables.
-    int p = start, q = mid + 1;
+// Array A[] has the items to sort; array B[] is a work array.
+void TopDownMergeSort(int A[], int B[], int n)
+{
+    CopyArray(A, 0, n, B);           // one time copy of A[] to B[]
+    TopDownSplitMerge(B, 0, n, A);   // sort data from B[] into A[]
+}
 
-    vector<int> Arr(end - start + 1);
-    int k = 0;
+// Sort the given run of array A[] using array B[] as a source.
+// iBegin is inclusive; iEnd is exclusive (A[iEnd] is not in the set).
+void TopDownSplitMerge(int B[], int iBegin, int iEnd, int A[])
+{
+    if (iEnd - iBegin < 2)                       // if run size == 1
+        return;                                 //   consider it sorted
+    // split the run longer than 1 item into halves
+    int iMiddle = (iEnd + iBegin) / 2;              // iMiddle = mid point
+    // recursively sort both runs from array A[] into B[]
+    TopDownSplitMerge(A, iBegin, iMiddle, B);  // sort the left  run
+    TopDownSplitMerge(A, iMiddle, iEnd, B);  // sort the right run
+    // merge the resulting runs from array B[] into A[]
+    TopDownMerge(B, iBegin, iMiddle, iEnd, A);
+}
 
-    for (int i = start; i <= end; i++) {
-        if (p > mid)      //checks if first part comes to an end or not .
-            Arr[k++] = A[q++];
+//  Left source half is A[ iBegin:iMiddle-1].
+// Right source half is A[iMiddle:iEnd-1   ].
+// Result is            B[ iBegin:iEnd-1   ].
+void TopDownMerge(int A[], int iBegin, int iMiddle, int iEnd, int B[])
+{
+    int i = iBegin, j = iMiddle;
 
-        else if (q > end)   //checks if second part comes to an end or not
-            Arr[k++] = A[p++];
-
-        else if (A[p] < A[q])     //checks which part has smaller element.
-            Arr[k++] = A[p++];
-
-        else
-            Arr[k++] = A[q++];
-    }
-    for (int p = 0; p < k; p++) {
-        /* Now the real array has elements in sorted manner including both
-             parts.*/
-        A[start++] = Arr[p];
+    // While there are elements in the left or right runs...
+    for (int k = iBegin; k < iEnd; k++) {
+        // If left run head exists and is <= existing right run head.
+        if (i < iMiddle && (j >= iEnd || A[i] <= A[j])) {
+            B[k] = A[i];
+            i = i + 1;
+        }
+        else {
+            B[k] = A[j];
+            j = j + 1;
+        }
     }
 }
 
-void merge_sort(vector<int> &A, int start, int end)
+void CopyArray(int A[], int iBegin, int iEnd, int B[])
 {
-    if (start < end) {
-        int mid = (start + end) / 2;           // defines the current array in 2 parts .
-        merge_sort(A, start, mid);                 // sort the 1st part of array .
-        merge_sort(A, mid + 1, end);              // sort the 2nd part of array.
-
-      // merge the both parts by comparing elements of both the parts.
-        merge(A, start, mid, end);
-    }
-}
-
-void merge_sort_helper(vector<int>& A)
-{
-    merge_sort(A, 0, A.size() - 1);
+    for (int k = iBegin; k < iEnd; k++)
+        B[k] = A[k];
 }
